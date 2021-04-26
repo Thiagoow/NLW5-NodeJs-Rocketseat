38c26e47afc1baaabe34e6674dcfab73de326205
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-04-26 17:54:01 -0300

    Aula 04 (1h4min) bug concluído 😎

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -3,6 +3,7 @@ import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
 import { MessagesService } from "../services/MessagesService";
 
+//Força ser necessário receber APENAS um texto e um email:
 interface IParams {
   text: string;
   email: string;
@@ -15,7 +16,8 @@ io.on("connect", (socket) => {
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
-    const { text, email } = params;
+    //Força ser necessário receber APENAS um texto e um email:
+    const { text, email } = params as IParams;
     let user_id = null;
 
     const userExists = await usersService.findByEmail(email);
