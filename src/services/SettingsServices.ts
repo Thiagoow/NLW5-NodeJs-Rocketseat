5109ab452d251a2d3039e1112ca71d0524fae9a8
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-04-26 18:20:10 -0300

    AULA 04 FINALIZADA FINALMENTE AAA ASUAHSAUS

--- src/services/SettingsServices.ts
+++ src/services/SettingsServices.ts
@@ -35,6 +35,24 @@ class SettingsService {
 
     return settings;
   }
+
+  async findByUsername(username: string) {
+    const settings = await this.settingsRepository.findOne({
+      username,
+    });
+    return settings;
+  }
+
+  async update(username: string, chat: boolean) {
+    await this.settingsRepository
+      .createQueryBuilder()
+      .update(Setting)
+      .set({ chat })
+      .where("username = :username", {
+        username,
+      })
+      .execute();
+  }
 }
 
 export { SettingsService };
